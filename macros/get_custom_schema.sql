133196b65c826c616a7f0be9e748533ef12635d1
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-02-26 23:46:38 +0000

    add custom schema config

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -1,5 +1,19 @@
 -- put this in macros/get_custom_schema.sql
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
+    
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        --{{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {{ custom_schema_name }}
+
+    {%- endif -%}
+
     {{ generate_schema_name_for_env(custom_schema_name, node) }}
 {%- endmacro %}
