5e76b38334ea8078810e542017a3715c17db4db6
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-02-26 23:50:01 +0000

    custom schema simplification

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -2,18 +2,6 @@
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
     
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
-        --{{ default_schema }}_{{ custom_schema_name | trim }}
-
-        {{ custom_schema_name }}
-
-    {%- endif -%}
-
     {{ generate_schema_name_for_env(custom_schema_name, node) }}
+
 {%- endmacro %}
