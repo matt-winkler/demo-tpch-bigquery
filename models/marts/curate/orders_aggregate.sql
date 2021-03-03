0681043d173b4e968f02da6ba8e9685dc62e0e95
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-03-03 16:44:15 +0000

    add multiple schemas

---
+++ models/marts/curate/orders_aggregate.sql
@@ -0,0 +1,11 @@
+with orders as ( select * from {{ ref('order_items') }} ),
+
+aggregated as (
+
+    select order_key,
+           SUM(gross_item_sales_amount) as gross_order_sales_amount
+    from   orders 
+    group by 1
+)
+
+select * from aggregated
